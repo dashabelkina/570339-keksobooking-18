e56468b97d6078840c036cf2d0e689cfdded2378
dasha <37762064+dashabelkina@users.noreply.github.com>
2019-09-24 14:50:46 +0700

    Исправление замечаний

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 'use strict';
 
 var offersCount = 8;
-var mockTitle = ['Заголовок01', 'Заголовок02', 'Заголовок03', 'Заголовок04', 'Заголовок05', 'Заголовок06', 'Заголовок07', 'Заголовок08'];
-var mockType = ['palace', 'flat', 'house', 'bungalo'];
-var mockCheckin = ['12:00', '13:00', '14:00'];
-var mockCheckout = ['12:00', '13:00', '14:00'];
+var mockTitles = ['Заголовок01', 'Заголовок02', 'Заголовок03', 'Заголовок04', 'Заголовок05', 'Заголовок06', 'Заголовок07', 'Заголовок08'];
+var mockTypes = ['palace', 'flat', 'house', 'bungalo'];
+var mockCheckins = ['12:00', '13:00', '14:00'];
+var mockCheckouts = ['12:00', '13:00', '14:00'];
 var mockFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var mockDescription = ['Описание01', 'Описание02', 'Описание03', 'Описание04', 'Описание05', 'Описание06', 'Описание07', 'Описание08'];
+var mockDescriptions = ['Описание01', 'Описание02', 'Описание03', 'Описание04', 'Описание05', 'Описание06', 'Описание07', 'Описание08'];
 var mockPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var minLocationY = 130;
-var maxLocationY = 630;
-var pinWidth = 50;
-var pinHeight = 70;
+var MIN_LOCATION_Y = 130;
+var MAX_LOCATION_Y = 630;
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
 
 var getRandomNumberInRange = function (min, max) {
   return Math.floor(Math.random() * max) + min;
@@ -30,23 +30,24 @@ var shuffleArray = function (array) {
 };
 
 var getMockOffer = function () {
-  var mock = [];
+  var mocks = [];
+
   for (var i = 0; i < offersCount; i++) {
     var avatar = 'img/avatars/user0' + (i + 1) + '.png';
-    var title = mockTitle[getRandomNumberInRange(0, mockTitle.length)];
+    var title = mockTitles[getRandomNumberInRange(0, mockTitles.length)];
     var price = getRandomNumberInRange(100, 100000);
-    var type = mockType[getRandomNumberInRange(0, mockType.length)];
+    var type = mockTypes[getRandomNumberInRange(0, mockTypes.length)];
     var rooms = getRandomNumberInRange(1, 15);
     var guests = getRandomNumberInRange(1, 30);
-    var checkin = mockCheckin[getRandomNumberInRange(0, mockCheckin.length)];
-    var checkout = mockCheckin[getRandomNumberInRange(0, mockCheckout.length)];
+    var checkin = mockCheckins[getRandomNumberInRange(0, mockCheckins.length)];
+    var checkout = mockCheckins[getRandomNumberInRange(0, mockCheckouts.length)];
     var features = shuffleArray(mockFeatures).slice(0, getRandomNumberInRange(0, mockFeatures.length));
-    var description = mockDescription[getRandomNumberInRange(0, mockDescription.length)];
+    var description = mockDescriptions[getRandomNumberInRange(0, mockDescriptions.length)];
     var photos = shuffleArray(mockPhotos).slice(0, getRandomNumberInRange(0, mockPhotos.length));
     var locationX = getRandomNumberInRange(0, map.clientWidth);
-    var locationY = getRandomNumberInRange(minLocationY, maxLocationY);
+    var locationY = getRandomNumberInRange(MIN_LOCATION_Y, MAX_LOCATION_Y);
 
-    mock.push({
+    mocks.push({
       author: {
         avatar: avatar,
       },
@@ -69,7 +70,7 @@ var getMockOffer = function () {
       }
     });
   }
-  return mock;
+  return mocks;
 };
 
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
@@ -79,7 +80,7 @@ var generatePin = function (pin) {
   var pinImage = pinElement.querySelector('img');
   var pinLocationX = pin.location.x;
   var pinLocationY = pin.location.y;
-  pinElement.style = 'left:' + (pinLocationX - pinWidth / 2) + 'px; top:' + (pinLocationY - pinHeight) + 'px';
+  pinElement.style = 'left:' + (pinLocationX - PIN_WIDTH / 2) + 'px; top:' + (pinLocationY - PIN_HEIGHT) + 'px';
   pinImage.src = pin.author.avatar;
   pinImage.alt = pin.offer.title;
   return pinElement;
