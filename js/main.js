ec6ab9232479cbe6c6585424f41a7a9991b22e8c
dasha <37762064+dashabelkina@users.noreply.github.com>
2019-09-30 19:11:08 +0700

    Личный проект: подробности

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 
 var OFFERS_COUNT = 8;
 var OFFERS_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var CHECKINS = ['12:00', '13:00', '14:00'];
 var CHECKOUTS = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
@@ -10,13 +9,15 @@ var MIN_LOCATION_Y = 130;
 var MAX_LOCATION_Y = 630;
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
+var PIN_PEAK_HEIGHT = 20;
 var PHOTOS_AMOUNT = 3;
-var OFFER_TYPES_TRANSLATE = {
+var OFFER_TYPES = {
   FLAT: 'Квартира',
   BUNGALO: 'Бунгало',
   HOUSE: 'Дом',
   PALACE: 'Дворец',
 };
+var ENTER_KEYCODE = 13;
 var wordsRoom = [' комната', ' комнаты', ' комнат'];
 
 var getRandomNumberInRange = function (min, max) {
@@ -111,7 +112,7 @@ var renderPins = function () {
 };
 
 var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+// map.classList.remove('map--faded');
 var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
 var mapFilter = map.querySelector('.map__filters-container');
 
@@ -123,10 +124,10 @@ var getDeclensionWordGuest = function (n) {
   return (n === 1) ? ' гостя' : ' гостей';
 };
 
-var liTpl = '<li class="popup__feature popup__feature--{{x}}"></li>';
-var imgTpl = '<img src="{{x}}" class="popup__photo" width="45" height="40" alt="Фотография жилья">';
+var featureTemplate = '<li class="popup__feature popup__feature--{{x}}"></li>';
+var photoTemplate = '<img src="{{x}}" class="popup__photo" width="45" height="40" alt="Фотография жилья">';
 
-var template = function (data, tpl) {
+var getTemplate = function (data, tpl) {
   var result = '';
   for (var i = 0; i < data.length; i++) {
     result += tpl.replace('{{x}}', data[i]);
@@ -146,12 +147,12 @@ var generateOfferCard = function (offerCards) {
   cardElement.querySelector('.popup__title').textContent = offerCards.offer.title;
   cardElement.querySelector('.popup__text--address').textContent = offerCards.offer.address;
   cardElement.querySelector('.popup__text--price').innerHTML = offerCards.offer.price + '&#x20bd;<span>/ночь</span>';
-  cardElement.querySelector('.popup__type').textContent = OFFER_TYPES_TRANSLATE[offerCards.offer.type];
+  cardElement.querySelector('.popup__type').textContent = OFFER_TYPES[offerCards.offer.type];
   cardElement.querySelector('.popup__text--capacity').textContent = getStringCapacity(offerCards.offer.rooms, offerCards.offer.guests);
   cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + offerCards.offer.checkin + ', выезд до ' + offerCards.offer.checkout;
-  cardElement.querySelector('.popup__features').innerHTML = template(offerCards.offer.features, liTpl);
+  cardElement.querySelector('.popup__features').innerHTML = getTemplate(offerCards.offer.features, featureTemplate);
   cardElement.querySelector('.popup__description').textContent = offerCards.offer.description;
-  cardElement.querySelector('.popup__photos').innerHTML = template(offerCards.offer.photos, imgTpl);
+  cardElement.querySelector('.popup__photos').innerHTML = getTemplate(offerCards.offer.photos, photoTemplate);
   cardElement.querySelector('.popup__avatar').setAttribute('src', offerCards.author.avatar);
 
   cardFragment.appendChild(cardElement);
@@ -159,5 +160,74 @@ var generateOfferCard = function (offerCards) {
 };
 
 var objList = getMockOffers(OFFERS_COUNT);
-renderPins(objList);
-generateOfferCard(objList[0]);
+var adForm = document.querySelector('.ad-form');
+var address = adForm.querySelector('#address');
+var mapFilters = document.querySelector('.map__filters');
+var adFormElements = adForm.querySelectorAll('.ad-form__element');
+var mapPinMain = document.querySelector('.map__pin--main');
+var adFormRoomsNumber = adForm.querySelector('#room_number');
+var adFormCapacity = adForm.querySelector('#capacity');
+// Функция для смены состояния элементов
+var reverseItemsState = function (arr, state) {
+  for (var i = 0; i < arr.length; i++) {
+    arr[i].disabled = state;
+  }
+};
+// Координаты пина
+var pinMainX = Math.round(parseInt(mapPinMain.style.left, 10) + PIN_WIDTH / 2);
+var pinMainY = Math.round(parseInt(mapPinMain.style.top, 10) + PIN_HEIGHT);
+var pinDeactivateMainY = Math.round(parseInt(mapPinMain.style.top, 10) + (PIN_HEIGHT - PIN_PEAK_HEIGHT) / 2);
+var getCoordinatesPinMain = function () {
+  return pinMainX + ', ' + pinMainY;
+};
+var getCoordinatesPinMainPeak = function () {
+  return pinMainX + ', ' + pinDeactivateMainY;
+};
+// Активное состояние страницы
+var activatePage = function () {
+  map.classList.remove('map--faded');
+  adForm.classList.remove('ad-form--disabled');
+  reverseItemsState(mapFilters, false);
+  reverseItemsState(adFormElements, false);
+  generateOfferCard(objList[0]);
+  renderPins(objList);
+  address.value = getCoordinatesPinMain();
+};
+// Неактивное состояние страницы
+var deactivatePage = function () {
+  map.classList.add('map--faded');
+  adForm.classList.add('ad-form--disabled');
+  reverseItemsState(mapFilters, true);
+  reverseItemsState(adFormElements, true);
+  address.value = getCoordinatesPinMainPeak();
+};
+// Исходное состояние страницы
+document.addEventListener('DOMContentLoaded', deactivatePage);
+// Перевод страницы в активное состояние при клике, срабатывает один раз
+mapPinMain.addEventListener('mouseup', function () {
+  activatePage();
+}, {once: true});
+// Перевод страницы в активное состояние при нажатии Enter
+mapPinMain.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    activatePage();
+  }
+});
+// Проверка соответствия количества гостей с количеством комнат
+var setRatioRoomsAndCapacity = function () {
+  var k = adFormRoomsNumber.value;
+  var j = adFormCapacity.value;
+  if (k < j) {
+    adFormCapacity.setCustomValidity('В тесноте, в духоте и на всех в обиде. Выберете больше комнат');
+  } else if (k >= j) {
+    adFormCapacity.setCustomValidity('');
+  }
+};
+
+adFormRoomsNumber.addEventListener('input', function () {
+  setRatioRoomsAndCapacity();
+});
+
+adFormCapacity.addEventListener('input', function () {
+  setRatioRoomsAndCapacity();
+});
